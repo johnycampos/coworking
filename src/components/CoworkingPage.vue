b8c1c03e58a4aff69f1c15ac2344d30beb51b467
johnycampos <johny.campos@ymail.com>
2025-07-21 22:03:09 -0300

    adicionar Fotos

--- src/components/CoworkingPage.vue
+++ src/components/CoworkingPage.vue
@@ -224,6 +224,10 @@ import image1 from '@/assets/1.jpg'
 import image2 from '@/assets/2.jpg'
 import image3 from '@/assets/3.jpg'
 import image4 from '@/assets/4.jpg'
+import image5 from '@/assets/5.jpg'
+import image6 from '@/assets/6.jpg'
+import image7 from '@/assets/7.jpg'
+
 
 const router = useRouter()
 const store = useReservationStore()
@@ -265,6 +269,18 @@ const galleryImages = ref([
   { 
     src: image4, 
     alt: 'Estações de trabalho individuais' 
+  },
+  { 
+    src: image5, 
+    alt: 'Cozinha embutida' 
+  },
+  { 
+    src: image6, 
+    alt: 'Bancada de escritório' 
+  },
+  { 
+    src: image7, 
+    alt: 'Banheiro privado' 
   }
 ])
 
